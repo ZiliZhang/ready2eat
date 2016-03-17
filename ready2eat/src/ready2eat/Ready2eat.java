a63d6c6b17cbe0773316321a5c2220424e25d42c
Zili Zhang <zhangzili0501@gmail.com>
2016-03-16 20:44:22 -0400

    initial commit

---
+++ ready2eat/src/ready2eat/Ready2eat.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ready2eat;
+import java.sql.* ;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Keren He, DoYeon Kim, Yu Qin, Zili Zhang 
+ */
+public class Ready2eat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException{
+        int sqlCode=0;      // Variable to hold SQLCODE
+        String sqlState="00000";  // Variable to hold SQLSTATE
+        
+        // Register the driver.You must register the driver before you can use it.
+        try {
+            DriverManager.registerDriver ( new org.postgresql.Driver() ) ;
+        } catch (Exception cnfe){
+            System.out.println("Class not found");
+        }
+        
+        String url = "jdbc:postgresql://comp421.cs.mcgill.ca:5432/cs421";
+        Connection con = DriverManager.getConnection (url,"cs421g08", "ready2eat") ;
+        Statement statement = con.createStatement ( ) ;
+         
+        try {
+	    String selectSQL = "SELECT * FROM dishes";
+	    System.out.println (selectSQL) ;
+	    statement.executeUpdate(selectSQL ) ;
+	}catch (SQLException e){
+                sqlCode = e.getErrorCode(); // Get SQLCODE
+                sqlState = e.getSQLState(); // Get SQLSTATE
+                
+                // Your code to handle errors comes here;
+                // something more meaningful than a print would be good
+                System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+        }
+
+    }
+    
+}
